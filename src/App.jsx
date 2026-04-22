a2b3b7de62d3ecad30b291255efc8462f184a2cb
Pings92 <pierick.jacquet@gmail.com>
2026-04-22 15:09:33 +0200

    portfolio skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    </>
+    <div className='min-h-screen overflow-x-hidden'>
+      <Navbar />
+      <main>
+
+      </main>
+    </div>
   )
 }
 
