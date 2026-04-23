737c6400b9fe64b7a62bb3e6e9a5a969be3cd53e
pierickj <pierick.jacquet@gmail>
2026-04-23 11:07:10 +0200

    background img

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,3 +1,15 @@
 export const Hero = () => {
-    return <section> </section>;
+    return ( 
+        <section className="relative min-h-screen flex items-center overflow-hidden"> 
+            {/* Background */}
+            <div className = "absolute inset-0">
+                <img src="public\projectImage\GGI2.png"
+                    alt="Image de fond"  
+                    // className="md:hidden glass-strong animate-fade-in"
+                    className="w-full h-full object-cover opacity-40"/>
+                <div className="absolute inset-0 bg-gradient-to-be from-background/20 viabackground/80 to-background"
+                />         
+            </div>
+        </section>
+    );
 }
