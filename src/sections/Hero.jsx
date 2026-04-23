8c583ff332bd9767c26b6a7af0abd3bb7517fdbe
pierickj <pierick.jacquet@gmail>
2026-04-23 16:12:09 +0200

    Animated button

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,4 +1,7 @@
 import react from "react";
+import { Button } from "@/components/button";
+import {ArrowRight, Download} from "lucide-react";
+import AnimatedBorderButton from "../components/AnimatedBorderButton";
 
 export const Hero = () => {
     return ( 
@@ -36,13 +39,95 @@ export const Hero = () => {
                     {/* Colonne de gauche contenu textuel*/}
                     <div className="space-y-8">
                         <div className="animate-fade-in">
-                            <span className="inline-flex items-center gap-2 px-4 py-2 rounded-full glass text-sm #e3c15b text-primary">
-                                <span className="w-2 h-2 #e3c15b bg-primary rounded-full animate-pulse" />
+                            <span className="inline-flex items-center gap-2 px-4 py-2 rounded-full glass text-sm  text-primary">
+                                <span className="w-2 h-2 bg-primary rounded-full animate-pulse" />
                                 Software
                             </span>
                         </div>
+                        <div>
+                            <h1 className="text-4x1 md:text-5xl lg:text-3xl font-bold leading-tight animate-fade-in animation-delay-100">
+                                Développeur <span className= "text-primary glow-text" >Fullstack</span>
+                                <br />
+                                Passionné par l'infini des possibilités,
+                                <br />
+                                <span className="font-serif italic font-normal text-white">
+                                du backend au frontend.
+                                </span>
+                                <br />
+                                <span className="text-secondary italic glow-text">
+                                No Mountain ain't too high, 
+                                </span>
+                                <br />
+                                <span className="text-secondary italic glow-text">
+                                no code ain't too complex.
+                                </span>
+                            </h1>
+                            <p className="text-lg text-muted-foreground max-w-lg animate-fade-in animation-delay-800">
+                                Hé c'est Piérick
+                                <br />
+                                Je sais compter deux par deux 
+                                <br />
+                                et lacer mes chaussures.
+                                <br />
+                                Je suis là pour jouer avec toi
+                                <br />
+                                Regarde moi grandir
+                                <br />Comme toi et moi
+
+                                <br />
+                                Me voici avec Mes amis
+                                <br />
+                                Ils ont des histoires
+                                <br />
+                                Ils ont tout leur temps
+                                Pour toi
+                                </p>
+                        </div>
+                        {/* Bouton d'appel à l'action */}
+                        <div>
+                            <Button
+                                href="#contact"
+                                size="lg"
+                                className="animate-fade-in animation-delay-800"
+                            >
+                                    Contactez-moi <ArrowRight className="w-5 h-5" />
+                            </Button>
+                            <AnimatedBorderButton 
+                                className="animate-fade-in animation-delay-800"
+                            />
+                        </div>
+                        {/* Réseaux sociaux */}
+                        <div className="flex items-center gap-4 animate-fade-in animation-delay-400">
+                            <span className="text-sm text-muted-foreground">Suivez-moi</span>
+                            {/* {[
+                                { icon Github, href: "http://github.com"},
+                                { icon Linkedin, href: "http://linkedin.com"},
+                                { icon Twitter, href: "http://twitter.com"},
+                            ].map((social, i) => (
+                                <a 
+                                    key={i}
+                                    href={social.href}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="p-2 rounded-full glass hover:bg-primary/10 hover:text-primary transition-all duration-300"
+                                >
+                                    {<social.icon className="w-5 h-5"/>}
+                                </a>
+                            ))} */}
+                        </div>
                     </div>
-                {/* Contenu de droite image */}
+                {/* Contenu de droite image profil */}
+                <div className="relative animate-fade-in animation-delay-300">
+                    {/* Image de profil */}
+                    <div className="relative max-w-md mx-auto ">
+                        <div className="relative glass rounded-3x1 p-2 glow-border">
+                            <img 
+                            src="public\projectImage\GGI.png" 
+                            alt="photo de profil" 
+                            className="w-full aspect-4/5 object-cover rounded-2x1" />
+                        </div>
+                    </div>
+                </div>
                 </div>
             </div>
         </section>
